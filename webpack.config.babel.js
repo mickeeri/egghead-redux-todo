736eaea33e8696a70d4a09964bebfc421555e690
me222wm <me222wm@student.lnu.se>
2016-07-04 21:46:27 +0200

    Indentation in webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -17,12 +17,15 @@ export default {
         include: __dirname,
       },
       {
-        test: /\.css$/, loader: 'style-loader!css-loader',
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
       },
       {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&minetype=application/font-woff' },
       {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader',
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
       },
     ],
   },
